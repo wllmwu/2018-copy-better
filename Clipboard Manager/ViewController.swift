17d4ea911b10809f4ce5b3e3ec76db32579c0202
William Wu <dev.wwu@gmail.com>
2018-06-11 10:10:09 -0700

    Initial Commit

---
+++ Clipboard Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clipboard Manager
+//
+//  Created by Bill Wu on 6/11/18.
+//  Copyright © 2018 William Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
