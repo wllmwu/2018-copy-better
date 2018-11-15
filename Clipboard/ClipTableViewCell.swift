89e14584e3018af26ee580200d620ef05312f19c
William Wu <dev.wwu@gmail.com>
2018-11-14 20:58:39 -0800

    Add classes for hopefully everything needed

---
+++ Clipboard/ClipTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClipTableViewCell.swift
+//  Clipboard
+//
+//  Created by Bill Wu on 11/14/18.
+//  Copyright © 2018 William Wu. All rights reserved.
+//
+
+import UIKit
+
+class ClipTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Clipboard/ClipViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ClipViewController.swift
+//  Clipboard
+//
+//  Created by Bill Wu on 11/14/18.
+//  Copyright © 2018 William Wu. All rights reserved.
+//
+
+import UIKit
+
+class ClipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
